268833af8c07d81077d39ae65c9c87481539a7ed
Aishwarya <aishpriya2516@gmail.com>
2025-01-13 23:55:25 +0530

    Adding the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # root 'calculators/index', to: 'calculators#index'
+  get 'calculators/index', to: 'calculators#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # post 'calculators/calculate', to: 'calculators#calculate'
+  resources :calculators, only:[:index] do
+    collection do
+      post :calculate
+    end
+  end
 end
